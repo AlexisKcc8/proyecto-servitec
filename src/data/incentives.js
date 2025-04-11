fcd81efbc37bb9f857bdc5155e13e5cfa3daa0e1
AlexisKcc8 <CodeKJS863@gmail.com>
2025-04-11 12:56:05 -0600

    feat: Implement incentives carousel with associated components and data

---
+++ src/data/incentives.js
@@ -0,0 +1,72 @@
+export const incentives = [
+  {
+    id: "crypto.randomUUID()",
+    title: "Calidad garantizada en cada compra.",
+    description:
+      "Disfruta de la tranquilidad de saber que cada producto ha sido cuidadosamente seleccionado para ofrecerte lo mejor en tecnología.",
+    icon: "calidad",
+  },
+  {
+    id: "crypto.randomUUID()",
+    title: "Asesoría experta y personalizada.",
+    description:
+      "Nuestro equipo de especialistas está aquí para guiarte en cada paso, asegurando que encuentres el producto ideal para tus necesidades.",
+    icon: "asesoria",
+  },
+  {
+    id: "crypto.randomUUID()",
+    title: "Tecnología de vanguardia a tu alcance.",
+    description:
+      "Accede a las últimas innovaciones en el mundo de la electrónica, todo en un solo lugar y a precios que no podrás resistir.",
+    icon: "tecnologia",
+  },
+  {
+    id: "crypto.randomUUID()",
+    title: "Precios que se ajustan a tu bolsillo.",
+    description:
+      "Descubre nuestras ofertas y promociones que hacen que la tecnología de alta calidad sea accesible para todos.",
+    icon: "precios",
+  },
+  {
+    id: "crypto.randomUUID()",
+    title: "Compra con confianza y seguridad.",
+    description:
+      "Nuestra plataforma está diseñada para brindarte una experiencia de compra segura, con opciones de pago flexibles y protección de tus datos.",
+    icon: "confianza",
+  },
+  {
+    id: "crypto.randomUUID()",
+    title: "Envíos rápidos y confiables.",
+    description:
+      "Sabemos que no puedes esperar para disfrutar de tus nuevos productos, por eso nos aseguramos de que lleguen a tus manos en el menor tiempo posible.",
+    icon: "envios",
+  },
+  {
+    id: "crypto.randomUUID()",
+    title: "Atención al cliente que supera tus expectativas.",
+    description:
+      "Estamos disponibles para resolver tus dudas y asistirte en cualquier momento, porque tu satisfacción es nuestra misión principal.",
+    icon: "atencion",
+  },
+  {
+    id: "crypto.randomUUID()",
+    title: "Amplia variedad para todos los gustos",
+    description:
+      "Desde los gadgets más populares hasta los accesorios más específicos, nuestra selección cubre todas tus necesidades tecnológicas.",
+    icon: "variedad",
+  },
+  {
+    id: "crypto.randomUUID()",
+    title: "Una experiencia de compra fluida y sin complicaciones",
+    description:
+      "Hemos simplificado cada paso para que puedas encontrar, comparar y comprar tus productos electrónicos de manera rápida y sencilla.",
+    icon: "compra",
+  },
+  {
+    id: "crypto.randomUUID()",
+    title: "Garantía de satisfacción total",
+    description:
+      "Queremos que estés completamente feliz con tu compra, por eso ofrecemos poiliticas de devolución y cambios que te respaldan en todo momento.",
+    icon: "satisfaccion",
+  },
+];
