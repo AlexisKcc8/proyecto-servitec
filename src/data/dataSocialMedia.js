638804ca4373afc18a306a63deeef4a01789fa71
AlexisKcc8 <CodeKJS863@gmail.com>
2025-07-30 16:23:41 -0600

    Refactor code structure for improved readability and maintainability

--- src/data/dataSocialMedia.js
+++ src/data/dataSocialMedia.js
@@ -2,14 +2,17 @@ export let socialMedia = [
   {
     name: "icon-facebook",
     url: "https://www.facebook.com/people/ServiTec/100064326272207/?mibextid=wwXIfr&rdid=s0qFfMM5wVraNIvP&share_url=https%3A%2F%2Fwww.facebook.com%2Fshare%2F15k1kPJJRd%2F%3Fmibextid%3DwwXIfr",
+    label: "Ir al Facebook de ServiTec",
   },
   {
     name: "icon-whatsapp",
     url: "https://api.whatsapp.com/send?phone=525656401126",
+    label: "Enviar mensaje por WhatsApp",
   },
 
   {
     name: "icon-tik-tok",
     url: "https://www.tiktok.com/@servitec19",
+    label: "Ir al TikTok de ServiTec",
   },
 ];
